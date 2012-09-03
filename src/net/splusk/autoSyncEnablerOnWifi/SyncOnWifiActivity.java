31f205740be19ea34a93a4194d5e84452577100d
shane <s.kakau@gmail.com>
2012-09-03 23:55:05 +0200

    initial src code commit

---
+++ src/net/splusk/autoSyncEnablerOnWifi/SyncOnWifiActivity.java
@@ -0,0 +1,74 @@
+package net.splusk.autoSyncEnablerOnWifi;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.widget.CheckBox;
+
+public class SyncOnWifiActivity extends Activity {
+
+    private static final int PREF_DISABLED = -1;
+    private static final int PREF_ENABLED = 1;
+	private static final String TAG = "ACCSYNCONWIFI";
+	private CheckBox onOff;
+	private SyncOnWifiStateReceiver receiver;
+	private IntentFilter intentFilter = new IntentFilter();
+	private int accSync = PREF_DISABLED;
+	private SharedPreferences mPrefs;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        onOff = (CheckBox) findViewById(R.id.on_off_option);
+        
+        mPrefs = getSharedPreferences(TAG, Context.MODE_PRIVATE);
+        accSync = mPrefs.getInt("accsync_setting", 0);
+        
+        if (accSync == PREF_DISABLED) {
+        	onOff.setChecked(false);
+        } else {
+        	onOff.setChecked(true);
+        }
+        
+        //FIXME: Only do if checked!!!!
+		intentFilter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+		receiver = new SyncOnWifiStateReceiver();
+		registerReceiver(receiver, intentFilter);
+
+    }
+    
+    /** register the BroadcastReceiver with the intent values to be matched */
+    /**@Override
+    public void onResume() {
+        super.onResume();
+        //FIXME: only do this if settings are set!!!
+		intentFilter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+		receiver = new SyncOnWifiStateReceiver();
+		registerReceiver(receiver, intentFilter);
+    }*/
+    
+    @Override
+    public void onPause() {
+        //Set preferences!!!!
+    	final SharedPreferences mPrefs = getSharedPreferences(TAG, Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        int state = onOff.isChecked() ? PREF_ENABLED : PREF_DISABLED;
+        prefsEditor.putInt("accsync_setting", state);
+        prefsEditor.commit();
+
+        super.onPause();
+    }
+    
+    @Override
+    public void onDestroy() {
+    	unregisterReceiver(receiver);
+    	super.onDestroy();
+    }
+    
+}
---
+++ src/net/splusk/autoSyncEnablerOnWifi/SyncOnWifiStateReceiver.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2011 Michael Kanis
+ * 
+ * This file is part of WifiSync.
+ *
+ * WifiSync is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * WifiSync is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with WifiSync.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// @ConQAT.Rating GREEN Hash: DC8C0A47067AD0F5C64D9DE93D637954
+
+package net.splusk.autoSyncEnablerOnWifi;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.NetworkInfo.State;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+
+/**
+ * A {@link BroadcastReceiver} that receives WiFi state change broadcasts and
+ * changes the auto sync setting accodring to the WiFi state.
+ */
+public class SyncOnWifiStateReceiver extends BroadcastReceiver {
+
+	/** The log tag. */
+	private static String TAG = "ACCSYNCONWIFI";
+	
+	/** Changes the auto sync setting when WiFi state changes. */
+	@Override
+	public void onReceive(final Context context, final Intent intent) {
+		
+		final String action = intent.getAction();
+
+		if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+
+			final NetworkInfo networkInfo = intent
+					.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+
+			if (networkInfo.getState().equals(State.CONNECTED)) {
+				enableSync();
+			} else if (networkInfo.getState().equals(State.DISCONNECTED)) {
+				disableSync();
+			}
+
+		} else if (action.equals(WifiManager.WIFI_STATE_CHANGED_ACTION)) {
+
+			final int wifiState = intent.getIntExtra(
+					WifiManager.EXTRA_WIFI_STATE,
+					WifiManager.WIFI_STATE_ENABLED);
+
+			if (wifiState == WifiManager.WIFI_STATE_DISABLED) {
+				disableSync();
+			} else if (wifiState == WifiManager.WIFI_STATE_ENABLED) {
+				enableSync();
+			}
+
+		}
+	}
+
+	/** Enables auto sync. */
+	private void enableSync() {
+		ContentResolver.setMasterSyncAutomatically(true);
+		Log.d(TAG, "Auto sync enabled.");
+	}
+
+	/** Disables auto sync. */
+	private void disableSync() {
+		ContentResolver.setMasterSyncAutomatically(false);
+		Log.d(TAG, "Auto sync disabled.");
+	}
+
+}
